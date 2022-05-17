fb99eb447034e54e3af2118461e85aa33f0889f5
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-05-16 20:50:50 -0400

    added event listener to menu icon on home page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// adding an event listener to open up navigation slide menu when user click on menu icon
+
+const formEl = document.querySelector('.menuIcon');
+
+formEl.addEventListener('click', () => {
+    console.log("you clicked the menuIcon");
+
+
+
+
+});
