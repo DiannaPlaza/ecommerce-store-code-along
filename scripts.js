d08f4eee13328cb6630dd74ce5549b3a8e0e6f17
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-05-17 09:17:07 -0400

    final commit

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-// adding an event listener to open up navigation slide menu when user clicks on menu icon
+// querying the DOM
 
 const formEl = document.querySelector('.menuIcon');
 const slideNav = document.querySelector(".slideOutNav");
 
 const close = document.querySelector ('.closeButton');
 
-
+// adding an event listener to open up navigation slide menu when user clicks on menu icon
 formEl.addEventListener('click', function openMenu() {
     
     slideNav.style.display = "block";
@@ -13,9 +13,17 @@ formEl.addEventListener('click', function openMenu() {
     
 });
 
-close.addEventListener('click', function closeMenu (){
+// adding an event listener to close navigation slide menu 
 
+close.addEventListener('click', function closeMenu (){
+    
     slideNav.style.display = "none";
     
 });
+
+// shop page
+
+
+
+
 
